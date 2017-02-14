a27a463dcbf9d1884ef7759583e820ad6a4807e9
Maxim <maxshaptala@gmail.com>
2017-02-14 02:56:15 +0200

    добавлен пример работы с dragndrop

---
+++ examples/qaexamples/src/main/java/org/itstep/qaexamples/DragAndDrop.java
@@ -0,0 +1,45 @@
+package org.itstep.qaexamples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by User on 14.02.2017.
+ */
+public class DragAndDrop {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String URL = "http://www.dhtmlx.com/docs/products/dhtmlxTree/index.shtml";
+
+        driver.get(URL);
+
+// It is always advisable to Maximize the window before performing DragNDrop action
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+
+        WebElement From = driver.findElement(By.xpath(".//*[@id='treebox1']/div/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[4]/span"));
+
+        WebElement To = driver.findElement(By.xpath(".//*[@id='treebox2']/div/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[4]/span"));
+
+        Actions builder = new Actions(driver);
+
+
+        Action dragAndDrop = builder.clickAndHold(From)
+                .moveToElement(To)
+                .release(To)
+                .build();
+
+        dragAndDrop.perform();
+    }
+}
