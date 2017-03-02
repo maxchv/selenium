40536d1aa95d995961654b5a5c802effc90641bc
Maxim <maxshaptala@gmail.com>
2017-03-02 21:16:31 +0200

    добавлен пример PageObject

---
+++ examples/usingarchetype2/src/test/java/org/itstep/JUnitTestSuite.java
@@ -0,0 +1,35 @@
+package org.itstep;
+
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import ru.stqa.selenium.factory.WebDriverPool;
+
+@RunWith(Suite.class)
+@SuiteClasses({SampleJUnitTest.class})
+public class JUnitTestSuite {
+
+    @Rule
+    public TestRule webDriverPool = new TestWatcher() {
+
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println("Success for " + description.getMethodName());
+            super.succeeded(description);
+        }
+
+        @Override
+        protected void finished(Description description) {
+            super.finished(description);
+            WebDriverPool.DEFAULT.dismissAll();
+
+        }
+
+        ;
+    };
+}
