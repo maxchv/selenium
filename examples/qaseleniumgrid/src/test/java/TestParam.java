fe296527ac4f2a790081b7f2edd5605a47bdd8ba
Maxim <maxshaptala@gmail.com>
2017-09-08 16:44:48 +0300

    added dependency to junit

---
+++ examples/qaseleniumgrid/src/test/java/TestParam.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class TestParam {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+
+    }
+}
